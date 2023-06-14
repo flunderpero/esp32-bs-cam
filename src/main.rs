2ab212e537cee7387abc743647a8e314cba58c49
Peter Romianowski <pero@cling.com>
2023-06-14 20:35:26 +0200

    [feat] Use LED to indicate errors during boot; Improve code

--- src/main.rs
+++ src/main.rs
@@ -3,27 +3,28 @@ use esp_idf_sys as _;
 use log::*;
 mod camera;
 mod net;
-use anyhow::{Error, Result};
-use esp_idf_svc::eventloop::EspSystemEventLoop;
+use anyhow::{Context, Result};
+use esp_idf_hal::gpio::*;
 use esp_idf_hal::prelude::Peripherals;
+use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition};
 use std::{thread::sleep, time::Duration};
 
-fn main() {
+fn main() -> Result<()> {
     esp_idf_sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
     info!("Starting up ...");
-    let peripherals = Peripherals::take().unwrap();
-    let sys_loop = EspSystemEventLoop::take().unwrap();
-    net::init(sys_loop, peripherals).or_else(error).unwrap();
-    camera::init().or_else(error).unwrap();
+    let sys_loop = EspSystemEventLoop::take().context("Failed to get `sys_loop`")?;
+    let peripherals = Peripherals::take().context("Failed to get `peripherals`")?;
+
+    let nvs = EspDefaultNvsPartition::take()?;
+    let mut led = PinDriver::output(peripherals.pins.gpio4)?;
+    led.set_high()?;
+    let _wifi = net::init(sys_loop.clone(), peripherals.modem, nvs.clone())?;
+    camera::init()?;
+    led.set_low()?;
     info!("Ready");
     loop {
         info!("Just sleeping ...");
         sleep(Duration::new(10, 0));
     }
-}
-
-fn error(err: Error) -> Result<()> {
-    error!("Startup failed: {}", err);
-    return Err(err);
 }
--- src/net.rs
+++ src/net.rs
@@ -1,30 +1,33 @@
 use anyhow::{Result};
 use embedded_svc::wifi::{ClientConfiguration, Configuration};
-use esp_idf_hal::prelude::Peripherals;
+use esp_idf_hal::{peripheral::Peripheral};
 use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition, wifi::EspWifi};
 use log::info;
 use std::{thread::sleep, time::Duration};
 
-pub fn init(sys_loop: EspSystemEventLoop, peripherals: Peripherals) -> Result<()> {
+pub fn init(
+    sys_loop: EspSystemEventLoop,
+    modem: impl Peripheral<P = esp_idf_hal::modem::Modem> + 'static,
+    nvs: EspDefaultNvsPartition
+) -> Result<Box<EspWifi<'static>>> {
     let wifi_ssid: &'static str = env!("BS_WIFI_SSID");
     let wifi_psk: &'static str = env!("BS_WIFI_PSK");
     info!("Connecting to WiFi...");
-    let nvs = EspDefaultNvsPartition::take()?;
-    let mut wifi_driver = EspWifi::new(peripherals.modem, sys_loop, Some(nvs))?;
-    wifi_driver.set_configuration(&Configuration::Client(ClientConfiguration {
+    let mut wifi = EspWifi::new(modem, sys_loop, Some(nvs))?;
+    wifi.set_configuration(&Configuration::Client(ClientConfiguration {
         ssid: wifi_ssid.into(),
         password: wifi_psk.into(),
         ..Default::default()
     }))?;
-    wifi_driver.start()?;
-    wifi_driver.connect()?;
-    while !wifi_driver.is_connected()? {
-        let config = wifi_driver.get_configuration()?;
+    wifi.start()?;
+    wifi.connect()?;
+    while !wifi.is_connected()? {
+        let config = wifi.get_configuration()?;
         info!("Waiting for WiFi connection: {:?}", config);
         sleep(Duration::new(1, 0));
     }
     loop {
-        let ip_info = wifi_driver.sta_netif().get_ip_info()?;
+        let ip_info = wifi.sta_netif().get_ip_info()?;
         info!("Waiting for IP address: {:?}", ip_info);
         if !ip_info.ip.is_unspecified() {
             break;
@@ -32,5 +35,5 @@ pub fn init(sys_loop: EspSystemEventLoop, peripherals: Peripherals) -> Result<()
         sleep(Duration::new(1, 0));
     }
     info!("WiFi connection established");
-    return Ok({});
+    return Ok(Box::new(wifi));
 }

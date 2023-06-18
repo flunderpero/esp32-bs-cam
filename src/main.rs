8d3b304399abf82075c547541d38f845c9fe2d23
Peter Romianowski <pero@cling.com>
2023-06-18 11:10:45 +0200

    [feat] Improve error handling and robustness; Improve `/info` endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
 use chrono::NaiveDateTime;
 use embedded_svc::{http::Method, io::Write};
 use esp_idf_hal::gpio::*;
@@ -8,9 +8,12 @@ use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition};
 use esp_idf_sys::{self as _, time, time_t};
 use log::*;
 use std::sync::{Arc, Mutex};
-use std::{ptr, thread::sleep, time::Duration};
+use std::{ptr, thread::sleep, time::Duration, ops::Deref};
+use serde::{Serialize};
 mod camera;
 mod net;
+
+const ERROR_THRESHOLD: u32 = 100;
 
 fn main() {
     esp_idf_sys::link_patches();
@@ -26,90 +29,121 @@ fn main() {
     camera::init().unwrap();
     let _sntp = net::sntp().unwrap();
     let statistics = Arc::new(Mutex::new(Statistics {
-        last_capture_date_time: None,
+        startup_at: now(),
+        last_capture_at: None,
         last_capture_name: None,
+        last_capture: None,
         capture_count: 0,
+        capture_count_since_last_error: 0,
+        last_error: None,
+        error_count: 0,
     }));
     let _http_server = setup_http_server(statistics.clone()).unwrap();
-    let mut uploader = net::Uploader::create().unwrap();
     // Setup is done.
     info!("Ready");
     led.set_high().unwrap();
-    let mut last_capture_secs = now().unwrap().timestamp();
+    loop {
+        if let Err(error) = main_loop(statistics.clone()) {
+            let mut stats = statistics.lock().unwrap();
+            stats.error_count += 1;
+            stats.capture_count_since_last_error =  0;
+            if stats.error_count >= ERROR_THRESHOLD {
+                panic!("There have been too many errors, restarting");
+            }
+            info!("That's an error - resuming operation: {}", error);
+            stats.last_error = Some(format!("[{}]: {}", iso_format(now()), error));
+            sleep(Duration::from_secs(2));
+        }
+    }
+}
+
+fn main_loop(statistics: Arc<Mutex<Statistics>>) -> Result<()> {
+    let mut uploader = net::Uploader::create().unwrap();
+    let mut last_capture_at = now();
     loop {
         // We want to capture at most 1 image per second.
-        while now().unwrap().timestamp() - last_capture_secs < 1 {
+        while now() - last_capture_at < 1 {
             info!("Wow, we're too fast, waiting a bit");
             sleep(Duration::from_millis(100));
         }
-        let now = now().unwrap();
-        last_capture_secs = now.timestamp();
-        let data = camera::capture_image().unwrap();
-        let name = format!("test_{}.jpg", iso_format(now));
-        uploader.upload(&data[..], name.as_str()).unwrap();
+        last_capture_at = now();
+        let data = camera::capture_image()?;
+        let name = format!("test_{}.jpg", iso_format(last_capture_at));
+        uploader.upload(&data[..], name.as_str())?;
         let mut stats = statistics.lock().unwrap();
+        stats.last_capture = Some(data);
         stats.capture_count += 1;
-        stats.last_capture_date_time = Some(now);
+        stats.capture_count_since_last_error += 1;
+        stats.last_capture_at = Some(last_capture_at);
         stats.last_capture_name = Some(name);
     }
 }
 
+#[derive(Serialize)]
 struct Statistics {
-    last_capture_date_time: Option<NaiveDateTime>,
+    startup_at: i64,
+    last_capture_at: Option<i64>,
     last_capture_name: Option<String>,
+    #[serde(skip_serializing)]
+    last_capture: Option<Vec<u8>>,
     capture_count: u32,
+    capture_count_since_last_error: u32,
+    last_error: Option<String>,
+    error_count: u32,
 }
 
 fn setup_http_server(statistics: Arc<Mutex<Statistics>>) -> Result<EspHttpServer> {
     let mut server = EspHttpServer::new(&Default::default())?;
+    let stats1 = statistics.clone();
     server.fn_handler("/info", Method::Get, move |req| {
         let headers = [
             ("content-type", "application/json"),
             ("connection", "close"),
         ];
         let mut res = req.into_response(200, None, &headers)?;
-        let stats = statistics.lock().unwrap();
-        let last_capture_date_time = match stats.last_capture_date_time {
-            Some(date) => iso_format(date),
-            None => "".to_string(),
-        };
-        let now = iso_format(now()?);
-        let json = format!(
-            "{{\n\"date_time\": \"{}\"\n\"last_capture_date_time\": \"{}\"\n\"capture_count\": {}\n}}",
-            now, last_capture_date_time, stats.capture_count
-        );
+        let stats = stats1.lock().unwrap();
+        let json = serde_json::to_string_pretty(&stats.deref())?;
         res.write_all(json.as_bytes())?;
         res.flush()?;
         res.release();
         Ok(())
     })?;
     server.fn_handler("/image", Method::Get, move |req| {
-        let data = &camera::capture_image()?[..];
-        let content_length_header = format!("{}", data.len());
-        let headers = [
-            ("content-type", "image/jpeg"),
-            ("connection", "close"),
-            ("content-length", &*content_length_header),
-        ];
-        let mut res = req.into_response(200, None, &headers)?;
-        res.write_all(data)?;
-        res.flush()?;
-        res.release();
+        let stats = statistics.lock().unwrap();
+        let data = stats.last_capture.clone();
+        match data {
+            Some(data) => {
+                let content_length_header = format!("{}", data.len());
+                let headers = [
+                    ("content-type", "image/jpeg"),
+                    ("connection", "close"),
+                    ("content-length", &*content_length_header),
+                ];
+                let mut res = req.into_response(200, None, &headers)?;
+                res.write_all(&data)?;
+                res.flush()?;
+            }
+            None => {
+                let headers = [("content-type", "text/plain"), ("connection", "close")];
+                let mut res = req.into_response(200, None, &headers)?;
+                res.write(b"No image has been captured yet")?;
+                res.flush()?;
+            }
+        }
         Ok(())
     })?;
     Ok(server)
 }
 
-fn iso_format(date: NaiveDateTime) -> String {
-    date.format("%Y%m%dT%H%M%SZ").to_string()
+fn iso_format(timestamp: i64) -> String {
+    let date = NaiveDateTime::from_timestamp_opt(timestamp, 0);
+    match date {
+        Some(date) => date.format("%Y%m%dT%H%M%SZ").to_string(),
+        None => "<invalid date>".to_owned(),
+    }
 }
 
-fn now() -> Result<NaiveDateTime> {
-    unsafe {
-        let timer: *mut time_t = ptr::null_mut();
-        let timestamp = time(timer);
-        let date_time = NaiveDateTime::from_timestamp_opt(timestamp as i64, 0)
-            .context("Unable to parse timestamp")?;
-        Ok(date_time)
-    }
+fn now() -> i64 {
+    let timer: *mut time_t = ptr::null_mut();
+    unsafe { time(timer) as i64 }
 }
